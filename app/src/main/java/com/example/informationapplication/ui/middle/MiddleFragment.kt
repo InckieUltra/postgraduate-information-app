785b1b3f07c66fba90a7a0b0b76b377477ec485c
Liadrid <1506227085@qq.com>
2022-11-29 19:16:29 +0800

    添加功能：点击日历选择日期

--- app/src/main/java/com/example/informationapplication/ui/middle/MiddleFragment.kt
+++ app/src/main/java/com/example/informationapplication/ui/middle/MiddleFragment.kt
@@ -1,16 +1,20 @@
 package com.example.informationapplication.ui.middle
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.informationapplication.databinding.FragmentMiddleBinding
+import java.util.*
+import kotlin.math.log
 
 class MiddleFragment : Fragment() {
-
     private var _binding: FragmentMiddleBinding? = null
 
     // This property is only valid between onCreateView and
@@ -28,10 +32,31 @@ class MiddleFragment : Fragment() {
         _binding = FragmentMiddleBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        middleViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val textView: TextView = binding.currentDateText
+        // choosing a certain date on calendarView
+        val today: Date = DatesUtil.getDayBegin()
+        val calendarView: CalendarView = binding.calendarView
+        calendarView.setOnDateChangeListener {
+                calendarView, year, month, day ->
+                    val toast: Toast = Toast.makeText(calendarView.context, "您选择了" + year + "年" + month + "月" + day + "日。", Toast.LENGTH_SHORT)
+                    toast.show()
+
+                    val cal: Calendar = Calendar.getInstance()
+                    cal.set(year, month, day)
+                    val currentChosenDate: Date = cal.time
+                    val diff = DatesUtil.getDiffDays(currentChosenDate, today)
+                    textView.text = (when{
+                        diff > 2 -> diff.toString() + "天后"
+                        diff < -2 -> (-diff).toString() + "天前"
+                        diff == 0 -> "今天"
+                        diff == 1 -> "明天"
+                        diff == 2 -> "后天"
+                        diff == -1 -> "昨天"
+                        diff == -2 -> "前天"
+                        else -> ""
+                    })
         }
+
         return root
     }
 
---
+++ app/src/main/java/com/example/informationapplication/ui/middle/Schedual.kt
@@ -0,0 +1,10 @@
+package com.example.informationapplication.ui.middle
+
+import java.util.*
+
+class Schedual {
+    private var title : String = "新建日程"
+    private var content : String = "准备做什么？"
+    private lateinit var date : Date
+    private var color : String = "default"
+}
