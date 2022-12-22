3a15d73c0f4a080d11f2d8990ee513e640b7bf0c
Liadrid <1506227085@qq.com>
2022-12-22 20:37:08 +0800

    综合优化

--- app/src/main/java/com/example/informationapplication/ui/middle/entity/Schedule.kt
+++ app/src/main/java/com/example/informationapplication/ui/middle/entity/Schedule.kt
@@ -6,7 +6,7 @@ import java.util.*
 class Schedule {
     var id : Int? = null
     var title : String = "新建日程"
-    var content : String = "准备做什么？"
+    var content : String = ""
     lateinit var date : Date
     var tag : String = R.id.box1.toString()
 }
